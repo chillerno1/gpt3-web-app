2769139858bc836e539a31d4d4749e41afa87ae4
chillerno1 <gh.chiller@pm.me>
2021-12-21 22:14:37 +1100

    setup :: basic css structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css';
+
 const App = () => {
     return (
-        <div>
-            Hello world!
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility /> 
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
